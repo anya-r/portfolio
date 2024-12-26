f42a59e52fcd920ac008d6fe34073eb05fd0d627
Anya Raju <anyaraju@Anyas-Air-2.attlocal.net>
2024-12-26 16:27:35 -0600

    establishing homepage

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+export default function Home(){
+    return(
+        <div>
+            <div>homepage</div>
+
+
+            <div className="flex bg-red-300" >
+
+            <div class="flex bg-gray-100 w-32 p-4">Sidebar</div>
+
+                
+            </div>
+
+        </div>
+        
+    )
+}
